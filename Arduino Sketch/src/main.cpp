38d5c24ed53b5c0cd92ea2322e40bad7b076162c
Michael Topple <gm5aug@topple.scot>
2024-05-29 17:16:27 +0100

    Update main.cpp

--- Arduino Sketch/src/main.cpp
+++ Arduino Sketch/src/main.cpp
@@ -16,7 +16,6 @@ Frequency offset must be configured for reliable decoding. At present time, ther
 #include <RadioLib.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Wifi.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
